a45c5e545be15b177492c1cb1f25591f31af4d7c
Sandara <20255042@s.ubaguio.edu>
2026-02-03 16:48:26 +0800

    Update project files

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import Header from "./Header";
 import Student from "./Student";
 import Footer from "./Footer";
 import Button from "./Button";
+import { useState } from 'react'
 import './App.css';
 
 function App() {
 const handleSave = () => alert("Saved!");
 const handleCancel = () => alert("Cancelled!");
+const [count, setCount] = useState(0)
+
 
 return (
 <>
@@ -17,6 +20,9 @@ return (
 <Student name="Erika Mae Topacio" course="BSIT" />
 <Button label="Save" onClick={handleSave} />
 <Button label="Cancel" onClick={handleCancel} />
+<button onClick={() => setCount(count < 4 ? count + 1 : count)}>
+          count is {count}
+        </button>
 <Footer />
 </>
   );
---
+++ src/Header.tsx
@@ -0,0 +1,4 @@
+function Header() {
+return <h1>Here are my students</h1>;
+}
+export default Header;
